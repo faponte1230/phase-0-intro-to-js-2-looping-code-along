bd7cf66377a6a3b07703d6027b766321c61a06a6
Fernando Aponte <faponte1230@yahoo.com>
2022-11-09 16:10:11 -0500

    commit tree cb9bf37e9fe673f3c126bc28369918cd4a1aa7cd

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+    function writeCards(names) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+      messages.push("Thank you, " + names[i] + ", for the wonderful surprise gift!")
+    }
+    return messages;
+  }
+
+  function countDown(){
+    let countDown = 10
+    while(countDown >=0){
+        console.log(countDown --)
+    }
+  }
